7c52b77629750c3ab8dc5e3437f3b848f911196a
harsh-agr79 <agrharsh7932@gmail.com>
2025-08-25 22:55:40 +0530

    Personalized Changes

--- app/Filament/Resources/Pollutions/Tables/PollutionsTable.php
+++ app/Filament/Resources/Pollutions/Tables/PollutionsTable.php
@@ -23,7 +23,7 @@ class PollutionsTable
                     ->searchable(),
                 TextColumn::make('lattitude')
                     ->searchable(),
-                ImageColumn::make('image')->disk('public')->imageHeight(100),
+                ImageColumn::make('image')->disk('public')->imageHeight(120),
                 TextColumn::make('env_reading')
                     ->searchable(),
                 TextColumn::make('analysis')
